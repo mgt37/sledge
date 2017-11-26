f35ddf8ae055061f7ced260466f1b6ef0dd9e66a
Michael Topp <mgtopp@gmail.com>
2017-11-26 09:24:04 +0000

    Third change to database url

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
 
 var url = process.env.DATABASEURL || "mongodb://localhost/sledge";
-//mongoose.connect(url);
+mongoose.connect(url);
 //mongoose.connect("mongodb://localhost/sledge", {useMongoClient: true} );
 
-mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect(process.env.DATABASEURL);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
