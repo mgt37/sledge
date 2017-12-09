faed9c11e71eea8aff7e868e594cb39958d1195b
Michael Topp <mgtopp@gmail.com>
2017-12-09 11:10:42 +0000

    trying to resolve error

--- app.js
+++ app.js
@@ -55,9 +55,9 @@ app.use(require("express-session")({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-/*passport.use(new LocalStrategy(User.authenticate()));
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());*/
+passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
