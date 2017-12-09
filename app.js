fa29128191112683f5746b82482727370909e3b7
Michael Topp <mgtopp@gmail.com>
2017-12-09 10:34:25 +0000

    testing removal of port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express        = require("express"),
     app            = express(),
-    port           = process.env.PORT || 8080,
+    /*port           = process.env.PORT || 8080,*/
     mongoose       = require("mongoose"),
     flash          = require("connect-flash"),
     morgan         = require('morgan'),
