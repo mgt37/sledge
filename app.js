b52f9dc2ca342d3b85dc4a351ae1d76bdd3b6874
Michael Topp <mgtopp@gmail.com>
2017-12-09 10:02:07 +0000

    fixed body parser error

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(flash());
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+/*app.use(bodyParser()); // get information from html forms*/
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
